531d64ebdb81bbc948cecdcc66788c5eee2df89f
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2019-05-19 10:52:47 +0100

    Initial commit based on Spring Initializr

---
+++ src/main/java/com/mikkri/mazecrawler/MazeCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.mikkri.mazecrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazeCrawlerApplication.class, args);
+	}
+
+}

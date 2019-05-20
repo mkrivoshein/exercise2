3b1c073c43fa6aecba2a2237801102ad8de9d1c0
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2019-05-20 11:16:13 +0100

    Set default profile to standalone

--- src/main/java/com/mikkri/mazecrawler/MazeCrawlerApplication.java
+++ src/main/java/com/mikkri/mazecrawler/MazeCrawlerApplication.java
@@ -2,12 +2,19 @@ package com.mikkri.mazecrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class MazeCrawlerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MazeCrawlerApplication.class, args);
+		SpringApplication application = new SpringApplication(MazeCrawlerApplication.class);
+		ConfigurableEnvironment environment = new StandardEnvironment();
+		// if this method is called, assume it is a standalone run rather than some unit test or executing
+		// within context of some other application
+		environment.setDefaultProfiles("standalone");
+		application.setEnvironment(environment);
+		application.run(args);
 	}
-
 }
